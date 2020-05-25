00b34eb722a04dffc65f87793001b7b8e795f733
Tony <32372239+TtonyxX@users.noreply.github.com>
2020-05-25 15:33:58 -0700

    styled and added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 
 app.use(express.json())
@@ -6,12 +7,7 @@ app.set('view-engine', 'ejs')
 
 const linesRouter = require('./routes/lines')
 app.use('/lines', linesRouter)
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { url: 'getpickuplines.herokuapp.com' })
