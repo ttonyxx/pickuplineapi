fc40fe954a518acb000c17e36202e9479af55048
Tony <32372239+TtonyxX@users.noreply.github.com>
2020-05-27 12:31:14 -0700

    Added a favicon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+var favicon = require('serve-favicon')
+var path = require('path')
 var cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.set('view-engine', 'ejs')
 
 const linesRouter = require('./routes/lines')
